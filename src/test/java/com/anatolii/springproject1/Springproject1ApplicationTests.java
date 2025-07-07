4fb697d87e49fc87a2a6820d46db2fb1bb7e6bca
Alyander <momoroot33@gmail.com>
2025-07-07 21:06:06 +0300

    Init

---
+++ src/test/java/com/anatolii/springproject1/Springproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anatolii.springproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
